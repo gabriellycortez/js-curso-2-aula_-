e73e3d507a71f501eb124e1f2364a885178f80c9
Gabrielly Cortez <cortezgabrielly@gmail.com>
2023-12-04 20:54:15 -0300

    adicionando funcoes

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-let titulo = document.querySelector('h1');
+/*let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do Número Secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';*/
 
- function verificarChute() {
-    console.log('O botão foi clicado!')
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+   let campo = document.querySelector(tag);
+   campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+
+function verificarChute() {
+   let chute = document.querySelector('input').value;
+    //console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto) {
+      console.log('Acertou');
+    } else {
+      console.log('Errou');
+    }
+ }
+
+ function gerarNumeroAleatorio() {
+   return parseInt(Math.random() * 10 + 1);
  }
